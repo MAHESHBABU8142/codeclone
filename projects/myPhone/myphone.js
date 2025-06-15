3506a3defdfe232638afcff9724f92f99cd1db1f
maheshbabu <mudavathmaheshbabu001@gmail.com>
2025-06-15 18:54:51 +0530

    home apps icons are added

--- projects/myPhone/myphone.js
+++ projects/myPhone/myphone.js
@@ -3,7 +3,7 @@ const now = new Date();
 
 document.querySelector("header #mini-time").textContent=`${now.getHours()-12}:${now.getMinutes()} PM`;
 document.querySelector("main #main-time").innerHTML=`0${now.getHours()-12}:${now.getMinutes()}<span>PM</span>`;
-document.querySelector("main #main-date").innerHTML=`sat, june ${now.getDate()}`;
+document.querySelector("main #main-date").innerHTML=`sun, june ${now.getDate()}`;
 
 let userInput=document.querySelector("main #search-bar input");
 userInput.addEventListener("keypress",function(event){
@@ -14,3 +14,21 @@ userInput.addEventListener("keypress",function(event){
       userInput.value="";
     }
 });
+
+
+const swipeArea = document.querySelector("body");
+
+  let startY = 0;
+
+  swipeArea.addEventListener("touchstart", (e) => {
+    startY = e.touches[0].clientY;
+  });
+
+  swipeArea.addEventListener("touchend", (e) => {
+    const endY = e.changedTouches[0].clientY;
+    const deltaY = startY - endY;
+
+    if (deltaY > 50) { // Minimum swipe distance to trigger
+       document.querySelector("body").innerHTML="<h1>here will display all apps<h1>";
+    }
+  });
